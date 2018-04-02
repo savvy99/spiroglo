8d9bebfa221681cd25522ce7598f9584701b8859
sarvjit <sarvjit@nanra.com>
2018-04-02 22:34:18 +0100

    Give indication of speed of finger movement

--- app/src/main/java/com/kavi/spiroglo/MainActivity.java
+++ app/src/main/java/com/kavi/spiroglo/MainActivity.java
@@ -1,9 +1,9 @@
 package com.kavi.spiroglo;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -13,8 +13,10 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView mainImage;
     private TextView myMessage;
-    private TextView xCood;
-    private TextView yCood;
+    private TextView txtSpeed;
+    private TextView txtDirection;
+
+    private SpeedCheck speedChecker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,46 +26,84 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        myMessage  = (TextView) findViewById(R.id.MyMessage);
-        xCood = (TextView) findViewById(R.id.Xcood);
-        yCood = (TextView) findViewById(R.id.Ycood);
-
         mainImage = (ImageView) findViewById(R.id.mainImage);
-        mainImage.setOnTouchListener(new View.OnTouchListener() {
+        myMessage  = (TextView) findViewById(R.id.MyMessage);
+        txtSpeed = (TextView) findViewById(R.id.txtSpeed);
+        txtDirection = (TextView) findViewById(R.id.txtDirection);
 
+        mainImage.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                boolean eventProcessed = false;
                 switch(event.getAction()) {
                     case(MotionEvent.ACTION_DOWN) :
-                        myMessage.setText("Hi, I'm Kavi!");
-                        xCood.setText("X: " + event.getX());
-                        yCood.setText("Y:" + event.getY());
-                        mainImage.invalidate();
-                        return true;
+                        //myMessage.setText("Hi, I'm Kavi!");
+                        //mainImage.invalidate();
+                        speedChecker.xNow = event.getX() + v.getLeft();
+                        speedChecker.yNow = event.getY() + v.getTop();
+                        eventProcessed = true;
+                        break;
                     case(MotionEvent.ACTION_MOVE) :
-                        myMessage.setText("That tickles!");
-                        xCood.setText("X: " + event.getX());
-                        yCood.setText("Y:" + event.getY());
-                        mainImage.invalidate();
-                        return true;
+                        //myMessage.setText("That tickles!");
+                        //mainImage.invalidate();
+                        speedChecker.xNow = event.getX() + v.getLeft();
+                        speedChecker.yNow = event.getY() + v.getTop();
+                        eventProcessed = true;
+                        break;
                     case(MotionEvent.ACTION_UP) :
-                        myMessage.setText("Try again!");
-                        mainImage.invalidate();
-                        return true;
+                        //myMessage.setText("Try again!");
+                        //mainImage.invalidate();
+                        speedChecker.xNow = event.getX() + v.getLeft();
+                        speedChecker.yNow = event.getY() + v.getTop();
+                        eventProcessed = true;
+                        break;
                 }
-                return false;
+                return eventProcessed;
             }
         });
 
-        mainImage.setOnDragListener(new View.OnDragListener() {
+        speedChecker = new SpeedCheck(10);
+        speedChecker.start();
+    }
 
+    private class SpeedCheck {
+
+        long delay;
+
+        private volatile double xNow;
+        private volatile double yNow;
+        private double xPrevious;
+        private double yPrevious;
+
+        private boolean stop = false;
+        private Handler tickEventHandler = new Handler();
+        private Runnable tickTockEvent = new Runnable() {
             @Override
-            public boolean onDrag(View v, DragEvent event) {
-                myMessage.setText("That tickles!");
-                xCood.setText("X: " + event.getX());
-                yCood.setText("Y:" + event.getY());
-                return false;
+            public void run() {
+                double dx = xNow - xPrevious;
+                double dy = yNow - yPrevious;
+                xPrevious = xNow;
+                yPrevious = yNow;
+                double distance = Math.sqrt(dx * dx + dy * dy);
+                txtSpeed.setText(Double.toString(distance));
+                mainImage.invalidate();
+                if(!stop) {
+                    tickEventHandler.postDelayed(this, delay);
+                }
             }
-        });
+        };
+
+        SpeedCheck(long delayMillis) {
+            this.delay = delayMillis;
+
+        }
+
+        void start() {
+            tickEventHandler.post(tickTockEvent);
+        }
+
+        void stop() {
+            stop = true;
+        }
     }
 }
